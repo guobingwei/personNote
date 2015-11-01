2c5f563300df32b662a781b96b1e6bdbf6a7c4fc
Gobby <guobingwei@aliyun.com>
2015-11-01 22:33:41 +0800

    线程初步

--- src/main/java/com/phn/utils/ThreadSafe.java
+++ src/main/java/com/phn/utils/ThreadSafe.java
@@ -3,5 +3,37 @@ package com.phn.utils;
 /**
  * Created by john on 2015/11/1.
  */
-public class ThreadSafe {
+public class ThreadSafe extends Thread{
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+                System.out.print(number);
+            }
+        }
+    }
+
+    public static void main(String [] args) {
+        /*new ReaderThread().start();
+        ready = true;
+        number = 42;*/
+
+        Demo demo = new Demo();
+        demo.start();
+        for(int i = 0; i < 20; i++) {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+
+    }
+}
+
+class Demo extends Thread {
+    public void run () {
+        for(int i = 0; i < 20; i++) {
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+    }
 }
