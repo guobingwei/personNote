e18b11ed641b4ea18fd20e4901c7663d4a794997
Gabby <guobingwei@aliyun.com>
2016-02-22 20:03:49 +0800

    dead thread

---
+++ src/main/java/com/study/thread/DeadLockDemo.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.study.thread;
+
+/**
+ * @author guobing
+ */
+public class DeadLockDemo {
+	private final String A = "A";
+	private final String B = "B";
+	
+	private void deadLock() {
+		Thread t1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				synchronized(A) {
+					try {
+						Thread.currentThread().sleep(2000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					synchronized(B) {
+						System.out.println("A");
+					}
+				}
+			}
+			
+		});
+		
+		Thread t2 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				synchronized(B) {
+					synchronized(A) {
+						System.out.println("2");
+					}
+				}
+			}
+			
+		});
+	}
+	
+	public static void main(String [] args) {
+		new DeadLockDemo().deadLock();
+	}
+}
