5a85138f9364c88de8d9a7008465f3754b185bcf
Gabby <guobingwei@aliyun.com>
2016-04-13 11:06:10 +0800

    原型模式

---
+++ src/main/java/com/study/DesignPattern/prototypePattern/Square.java
@@ -0,0 +1,15 @@
+package com.study.designPattern.prototypePattern;
+
+/**
+ * Created by guobing on 2016/4/13.
+ */
+public class Square extends Shape {
+    public Square() {
+        this.type = "Square";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
+}
