153443fba467b7350c7bf95bd32b69127a84cbc1
Gobby <guobingwei@aliyun.com>
2016-04-21 09:03:21 +0800

    fork join

---
+++ src/main/java/com/study/fork/join/CountTask.java
@@ -0,0 +1,65 @@
+package com.study.fork.join;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Created by john on 2016/4/21.
+ */
+public class CountTask extends RecursiveTask<Integer> {
+
+    private static final int THRESHOLD = 2; // 阈值
+    private int start;
+    private int end;
+
+    public CountTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+
+        // 如果任务足够小就直接计算
+        boolean canCompute = (end - start) <= THRESHOLD;
+        if(canCompute) {
+            for(int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            // 大于阈值， 分成两个任务计算
+            int middle = (start + end) / 2;
+            CountTask leftTask = new CountTask(start, middle);
+            CountTask rightTask = new CountTask(middle + 1, end);
+
+            leftTask.fork();
+            rightTask.fork();
+
+            // wait result
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+
+            sum = leftResult + rightResult;
+        }
+        return sum;
+    }
+
+    public static void main(String [] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+
+        CountTask countTask = new CountTask(1, 4);
+
+        Future<Integer> result = forkJoinPool.submit(countTask);
+
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+
+        } catch (ExecutionException e) {
+
+        }
+    }
+}
