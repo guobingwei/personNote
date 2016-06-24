e1a521f1072a4b8d8eec8326a2e5bd5a6d0ec7ee
guobing <guobingwei@aliyun.com>
2016-06-24 19:06:14 +0800

    add

--- src/main/java/com/study/algorithm/AddDigits.java
+++ src/main/java/com/study/algorithm/AddDigits.java
@@ -1,7 +1,19 @@
 package com.study.algorithm;
 
 /**
+ * Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2.
+ * Since 2 has only one digit, return it.
  * Created by guobingwei on 2016/6/24.
  */
 public class AddDigits {
+
+    // 这么简洁，我没想出来……
+    public int addDigits(int num) {
+        int res = num % 9;
+        return (res != 0 || num == 0) ? res : 9;
+    }
+
+    public static void main(String [] args) {
+        System.out.print(new AddDigits().addDigits(19));
+    }
 }
