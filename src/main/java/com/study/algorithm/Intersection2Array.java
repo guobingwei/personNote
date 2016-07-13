3d214cf9f34256260278cc20ef5b48e4d0364160
guobing <guobingwei@aliyun.com>
2016-07-13 19:18:15 +0800

    fs

--- src/main/java/com/study/algorithm/Intersection2Array.java
+++ src/main/java/com/study/algorithm/Intersection2Array.java
@@ -1,7 +1,34 @@
 package com.study.algorithm;
 
+import java.util.HashMap;
+
 /**
+ * 找出一个数组中的交集
  * Created by guobing.wei on 2016/7/13.
  */
 public class Intersection2Array {
+
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        int len = nums1.length > nums2.length ? nums1.length : nums2.length;
+        int [] result = new int[len];
+        if(nums1.length <= 0 || nums2.length <= 0) {
+            return result;
+        }
+
+        HashMap map = new HashMap();
+        for(int i = 0; i < nums1.length; i++) {
+
+        }
+        return result;
+    }
+
+    public static void  main(String [] args) {
+        int [] nums1 = {1};
+        int [] nums2 = {0};
+        int [] r = new Intersection2Array().intersect(nums1, nums2);
+        for(int i : r ) {
+            System.out.println(i);
+        }
+    }
 }
--- src/main/java/com/study/nio/ChannelTest.java
+++ src/main/java/com/study/nio/ChannelTest.java
@@ -1,7 +1,13 @@
 package com.study.nio;
 
 /**
+ * 通道
+ *
  * Created by guobingwei on 2016/7/4.
  */
 public class ChannelTest {
+
+    public void test() {
+        
+    }
 }
