c2e3436d32772d23027cac4666b569e7f89bbe0c
guobing <guobingwei@aliyun.com>
2016-07-29 18:34:27 +0800

    hours rob问题

--- src/main/java/com/study/algorithm/ReverseVowels.java
+++ src/main/java/com/study/algorithm/ReverseVowels.java
@@ -16,28 +16,117 @@ import java.util.List;
  */
 public class ReverseVowels {
 
-    public String reverseVowels(String s) {
-        /*List<char> list = new ArrayList<>();
+    /**
+     * 自己想出来的最普通的做法,代码量太多了
+     * @param s
+     * @return
+     */
+    public static String reverseVowels(String s) {
+        char [] ch = s.toCharArray();
+        int i = 0;
+        int j = ch.length - 1;
+
+        List list = new ArrayList();
         list.add('a');
         list.add('e');
-        list.add('i');
         list.add('o');
+        list.add('i');
         list.add('u');
-        int i = 0;
-        int j = s.length();
+        list.add('A');
+        list.add('E');
+        list.add('I');
+        list.add('O');
+        list.add('U');
         while(i < j) {
-            if(list.contains(s.charAt(i)) && list.contains(s.charAt(j))) {
-                char t = s.charAt(i);
-                s.charAt(i) = ;
-                s.charAt(i) = s.charAt(j);
-                s.charAt(j) = t;
-            } else if(list.contains(s.charAt(i)) && !list.contains(s.charAt(j))) {
+            if(list.contains(ch[i]) && list.contains(ch[j])) {
+                char t = ch[i];
+                ch[i] = ch[j];
+                ch[j] = t;
+                i++;
+                j--;
+            } else if(list.contains(ch[i]) && !list.contains(ch[j])) {
                 j--;
             } else {
                 i++;
             }
+        }
+        return new String(ch);
+    }
 
-        }*/
-        return s;
+    public static void main(String [] args) {
+        System.out.print(reverseVowelsMe("hello"));
+    }
+
+
+    /**
+     * 网上别人写的,beats 98%.
+     * 主要区别就是抛弃了list,每次查找的时间复杂度是
+     * switch case 效率高多了
+     * @param s
+     * @return
+     */
+    public static boolean isVowel(char a){
+        switch(a){
+            case ('a') : return true;
+            case ('e') : return true;
+            case ('i') : return true;
+            case ('o') : return true;
+            case ('u') : return true;
+            case ('A') : return true;
+            case ('E') : return true;
+            case ('I') : return true;
+            case ('O') : return true;
+            case ('U') : return true;
+            default : return false;
+        }
+    }
+
+    // beats 98%
+    public static String reverseVowels1(String s) {
+        if (s.length()<2) return s;
+
+        char[] tab = s.toCharArray();
+        int j = tab.length - 1;
+        int i = 0;
+
+        while( i < j ) {
+
+            if (!isVowel(tab[i]))
+                i++;
+            else {
+                while (j!=i && !isVowel(tab[j]))
+                    j--;
+
+                char temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        return new String(tab);
+    }
+
+    // beats 78%
+    public static String reverseVowelsMe(String s) {
+        char [] ch = s.toCharArray();
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i < j) {
+            if(isVowel(ch[i]) && isVowel(ch[j])) {
+                char t = ch[i];
+                ch[i] = ch[j];
+                ch[j] = t;
+                i++;
+                j--;
+            } else if(isVowel(ch[i]) && !isVowel(ch[j])) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+
+        return new String(ch);
     }
 }
--- src/main/java/com/study/algorithm/linkListCycle.java
+++ src/main/java/com/study/algorithm/linkListCycle.java
@@ -34,4 +34,35 @@ public class linkListCycle {
 
         return false;
     }
+
+    public boolean hasCycleNiuke(ListNode head) {
+        if(head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(fast == slow ) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasCycleLeetCode(ListNode head) {
+        if(head == null) return false;
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker == runner) return true;
+        }
+        return false;
+    }
 }
