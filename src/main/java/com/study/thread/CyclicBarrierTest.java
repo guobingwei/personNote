e977f133a9c36509b3ae7c42bd2d9dd9b62e051f
guobing <1596883444@qq.com>
2016-08-03 22:52:49 +0800

    yield/CyclicBarrier/countDownLatch/Semaphore用法详解

---
+++ src/main/java/com/study/thread/CyclicBarrierTest.java
@@ -0,0 +1,39 @@
+package com.study.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 当改成3的时候，线程一直运行。因为没达到3.不会停止。
+ * Created by guobing on 2016/8/3.
+ */
+public class CyclicBarrierTest {
+
+    static CyclicBarrier cb = new CyclicBarrier(3);
+
+    public static void main(String [] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // await告诉CyclicBarrier已经达到内存屏障
+                    cb.await();
+                    System.out.println(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        try {
+            cb.await();
+            System.out.println(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+}
