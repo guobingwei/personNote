5cd0c3cad7e917c4537163f7ce734657f168ecc4
weiguobing <weiguobing@meituan.com>
2020-09-20 22:18:10 +0800

    add log

--- src/main/java/com/study/algorithm/BitMap.java
+++ src/main/java/com/study/algorithm/BitMap.java
@@ -2,6 +2,7 @@ package com.study.algorithm;
 
 /**
  * 位图运算
+ * 海量数据中寻找某个数是否存在 布隆过滤器
  * Created with IntelliJ IDEA.
  * Created by weiguobing on 2020-09-18.   17:30
  * email : weiguobing@meituan.com
@@ -30,9 +31,17 @@ public class BitMap {
 		return (bytes[byteIndex] & (1 << bitIndex)) != 0;
 	}
 
+	/**
+	 * 将数字 A 的第 k 位设置为1：A = A | (1 << (k - 1))
+	 * 将数字 A 的第 k 位设置为0：A = A & ~(1 << (k - 1))
+	 * 检测数字 A 的第 k 位：A & (1 << (k - 1)) != 0
+	 * 用于理解bitmap中代码
+	 *
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		BitMap bitMap = new BitMap(32);
-		bitMap.set(16);
-		System.out.println(bitMap.get(16));
+		bitMap.set(7);
+		System.out.println(bitMap.get(7));
 	}
 }
